956d0375e759447698eb6ca24c6e1dc4dcbea607
MiltonSV <milton.mgalindo@gmail.com>
2020-09-10 05:46:21 -0600

    Agrege el numero primo y quite el palindromo

--- resolutionGuide1.py
+++ resolutionGuide1.py
@@ -2,7 +2,7 @@
 
 #El usuario nos indica la operacion a realizar y capturamos el error al ingresar cualquier trcla y cerramos el programa
 try:
-    opcion = int(input("Por favor ingrese el numero de la operacion que desea realizar\n1.Piramide\n2.Palindromo\n#.cualquier tecla para salir\nopcion: "))
+    opcion = int(input("Por favor ingrese el numero de la operacion que desea realizar\n1.Piramide\n2.Numero primo\n#.cualquier tecla para salir\nopcion: "))
 
 except:
     print("Adios")
@@ -34,23 +34,32 @@ def piramide(bloques):
         print("El numero tiene que ser mayor a 0")
 
 
-#Funcion palindromo
-def palindromo(cadena):
-    if str(cadena) == str(cadena)[::-1]:
-        print("Es palindromo")
+#Funcion numero primo
+def esPrimo(numero):
+    contador = 0;
+    for num in range(1,numero):
+        if numero%num == 0:
+            contador=contador+1
+    
+    if contador==1:
+        print("Es primo")
     else:
-        print("no es palindromo")
+        print("No es primo")
 
 
 #elegimos la funcion a utilizar y enviamos los valores de sus parametros
 if(opcion==1):
     try:
-        nBloques = int(input("Por favor ingrese el numero de bloques de su piramide:"))
+        nBloques = int(input("Por favor ingrese el numero de bloques de su piramide: "))
         piramide(nBloques)
     except:
         print("Ingreso un tipo de dato no valido, vuelva a iniciar el programa")
         quit()
 
 else:
-        palabra = input("Digite la palabra para verificar si es palindromo: ")
-        palindromo(palabra)
+        try:
+            numero = int(input("Digite un numero para determinar si es primo o no: "))
+            esPrimo(numero)
+        except:
+            print("Ingreso un tipo de dato no valido, vuelva a iniciar el programa")
+            quit()
